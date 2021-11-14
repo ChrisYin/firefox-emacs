1ee94cec912381f8842f89853ea533a6f95fe95e
Chris Yin <chrisyin.2vv@gmail.com>
2021-11-14 11:58:48 +0800

    Add emacs-like scroll feature

--- src/emacs-cntl.js
+++ src/emacs-cntl.js
@@ -11,6 +11,14 @@ function alt_command(body) {
         if (/[0-9]/.test(body)) {
                 body = body % 9 - 1;
                 tab_switch(body);
+        } else if (body == "n") {
+                browser.tabs.executeScript({
+                        code: "window.scrollBy(0, window.innerHeight/2)"
+                });
+        } else if (body == "p") {
+                browser.tabs.executeScript({
+                        code: "window.scrollBy(0, -window.innerHeight/2)"
+                });
         }
 };
 
@@ -18,8 +26,10 @@ browser.commands.onCommand.addListener(
         function command_parser(command) {
                 const prefix = command.split("-")[0];
                 const body = command.split("-")[1];
+                //console.log(prefix);
+                //console.log(body);
                 if (prefix == "alt") {
-                        alt_command(body)
+                        alt_command(body);
                 }
         }
 );
